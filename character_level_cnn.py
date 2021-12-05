3cad684aa15a59b9accb8f410f163163af234e28
Bowen <bowenlig@gmail.com>
2021-12-04 20:47:15 -0500

    finished training and testing on character_level_cnn

--- character_level_cnn.py
+++ character_level_cnn.py
@@ -7,14 +7,14 @@ class CharacterLevelCNN(Model):
     def __init__(self):
         super(CharacterLevelCNN, self).__init__()
         self.conv1 = layers.Conv1D(256, 7, activation='relu')
-        self.pool1 = layers.MaxPool1D(3)
+        self.pool1 = layers.MaxPool1D(2)
         self.conv2 = layers.Conv1D(256, 7, activation='relu')
-        self.pool2 = layers.MaxPool1D(3)
+        self.pool2 = layers.MaxPool1D(2)
         self.conv3 = layers.Conv1D(256, 3, activation='relu')
         self.conv4 = layers.Conv1D(256, 3, activation='relu')
         self.conv5 = layers.Conv1D(256, 3, activation='relu')
         self.conv6 = layers.Conv1D(256, 3, activation='relu')
-        self.pool6 = layers.MaxPool1D(3)
+        self.pool6 = layers.MaxPool1D(2)
         self.flatten = layers.Flatten()
         self.d1 = layers.Dense(1024)
         self.dropout1 = layers.Dropout(0.5)
@@ -43,21 +43,42 @@ class CharacterLevelCNN(Model):
 # define all accepted characters
 vocabulary = list("""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789""")
 
-# this method performs one hot encoding to a given text and returns a matrix of size (62, 256, 1)
+# this method performs one hot encoding to a given text and returns a matrix of size (62, 256)
 def transform_text_to_matrix(text):
     num_of_columns = 256
 
-    matrix = np.zeros((len(vocabulary), num_of_columns, 1))
+    matrix = np.zeros((len(vocabulary), num_of_columns))
 
     for index, char in enumerate(text):
         if char != ' ' and char != '\n':
             pos_in_vocabulary = vocabulary.index(char)
-            matrix[pos_in_vocabulary][index][0] = 1
+            matrix[pos_in_vocabulary][index] = 1
 
         if index == num_of_columns-1:
             break
 
     return matrix
+
+# function to train the model with tf.GradientTape
+@tf.function
+def train_step(text, labels):
+  with tf.GradientTape() as tape:
+    predictions = model(text, training=True)
+    loss = loss_object(labels, predictions)
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  train_loss(loss)
+  train_accuracy(labels, predictions)
+
+# function to test the model
+@tf.function
+def test_step(text, labels):
+  predictions = model(text, training=False)
+  t_loss = loss_object(labels, predictions)
+
+  test_loss(t_loss)
+  test_accuracy(labels, predictions)
 
 #*****************************************************************************************
 # beginning of script
@@ -66,7 +87,10 @@ def transform_text_to_matrix(text):
 
 # load positive data from csv
 pos_dataset_file = open('scraped_tweets_pos/iota_tweets_pruned.csv', 'r', encoding='utf-8')
-pos_dataset = pos_dataset_file.readlines()
+#TODO: remove the [:100] used for testing!
+pos_dataset = pos_dataset_file.readlines()[:100]
+
+print(len(pos_dataset))
 
 # TODO: load negative data from csv
 neg_dataset = []
@@ -82,29 +106,73 @@ neg_train = neg_dataset[:neg_split_index]
 neg_test = neg_dataset[neg_split_index:]
 
 # combine positive and negative sets to make train and test sets
-train_set = np.concatenate((pos_train, neg_train))
-test_set = np.concatenate((pos_test, neg_test))
+x_train_text = np.concatenate((pos_train, neg_train))
+x_test_text = np.concatenate((pos_test, neg_test))
 
 # create y_train, where 0 is neg, 1 is pos
-y_train = np.zeros(train_set.size)
+y_train = np.zeros(x_train_text.size)
 y_train[:len(pos_train)] = 1
 
 # create y_test, where 0 is neg, 1 is pos
-y_test = np.zeros(test_set.size)
+y_test = np.zeros(x_test_text.size)
 y_test[:len(pos_test)] = 1
 
-# transform train and test sets with one hot encoding
-train_set_encoded = []
+# transform train set with one hot encoding
+x_train = []
+for index, x_train_sentence in enumerate(x_train_text):
+    train_sentence_encoded = transform_text_to_matrix(x_train_sentence)
+    x_train.append(train_sentence_encoded)
 
-for index, train_sentence in enumerate(train_set):
-    train_sentence_matrix = transform_text_to_matrix(train_sentence)
-    #train_set_encoded.extend(train_sentence_matrix)
+# transform test set with one hot encoding
+x_test = []
+for index, x_test_sentence in enumerate(x_test_text):
+    test_sentence_encoded = transform_text_to_matrix(x_test_sentence)
+    x_test.append(test_sentence_encoded)
 
-print(len(train_set_encoded))
+# use tf.data to batch and shuffle dataset
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 
-# matrix = transform_text_to_matrix("ab ab ab")
-# print(matrix.shape)
+#*****************************************************************************************
+# selecting loss, accuracy metrics and optimzer
+#*****************************************************************************************
 
-# model = CharacterLevelCNN()
-# model.build((62, 256, 1))
-# model.summary()
+# choose optimizer and loss function for training
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam()
+
+# select train and test loss and accuracy for the model
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+#*****************************************************************************************
+# training CNN model
+#*****************************************************************************************
+
+# Create an instance of the model
+model = CharacterLevelCNN()
+EPOCHS = 5
+
+for epoch in range(EPOCHS):
+  # Reset the metrics at the start of the next epoch
+  train_loss.reset_states()
+  train_accuracy.reset_states()
+  test_loss.reset_states()
+  test_accuracy.reset_states()
+
+  for text, labels in train_ds:
+    train_step(text, labels)
+
+  for test_text, test_labels in test_ds:
+    test_step(test_text, test_labels)
+
+  print(
+    f'Epoch {epoch + 1}, '
+    f'Loss: {train_loss.result()}, '
+    f'Accuracy: {train_accuracy.result() * 100}, '
+    f'Test Loss: {test_loss.result()}, '
+    f'Test Accuracy: {test_accuracy.result() * 100}'
+  )
