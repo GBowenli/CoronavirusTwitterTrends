46129c41b88e1283ed9a8e22f08a40da0a0a2fb2
FloriaPeng <46766400+FloriaPeng@users.noreply.github.com>
2021-12-07 23:12:04 -0500

    added more linear models, and scraping for test set

--- MLP.py
+++ MLP.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn import metrics
 from sklearn.model_selection import KFold
-from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 
 import nltk
 from nltk.stem import PorterStemmer
@@ -20,8 +20,8 @@ nltk.download('averaged_perceptron_tagger')
 
 df_pos = numpy.loadtxt('./dataset/pos/pos_tweets_pruned_normal.csv', delimiter='\n', encoding='utf-8', dtype=str)
 df_neg = numpy.loadtxt('./dataset/neg/neg_tweets_pruned_normal.csv', delimiter='\n', encoding='utf-8', dtype=str)
-twitter_text = numpy.concatenate((df_pos[0:10000], df_neg[0:10000]), axis=0)
-twitter_cat = numpy.concatenate((numpy.ones(10000), numpy.zeros(10000)), axis=0)
+twitter_text = numpy.concatenate((df_pos[0:50000], df_neg[0:50000]), axis=0)
+twitter_cat = numpy.concatenate((numpy.ones(50000), numpy.zeros(50000)), axis=0)
 
 
 # Normalize data after vectorization process is finished
@@ -110,61 +110,55 @@ def count_vec_lemma(train_set, test_set):
 
 # ------------------------------ training ------------------------------
 accuracies = []
-clf = LogisticRegression(C=40.0, max_iter=1000, random_state=0)
 kf = KFold(n_splits=5, shuffle=True)
 for train_index, test_index in kf.split(twitter_text):
     vectors_train, vectors_test = count_vector(twitter_text[train_index], twitter_text[test_index])
-    clf.fit(vectors_train, twitter_cat[train_index])
+    clf = MLPClassifier(random_state=0, max_iter=300).fit(vectors_train, twitter_cat[train_index])
     accuracies.append(metrics.accuracy_score(twitter_cat[test_index], clf.predict(vectors_test)))
 
 print("The accuracy of count vectorizer without any pre-processing is: " + str(numpy.mean(accuracies)))
 
 accuracies = []
-clf = LogisticRegression(C=40.0, max_iter=1000, random_state=0)
 kf = KFold(n_splits=5, shuffle=True)
 for train_index, test_index in kf.split(twitter_text):
     vectors_train, vectors_test = tfidf_vector(twitter_text[train_index], twitter_text[test_index])
-    clf.fit(vectors_train, twitter_cat[train_index])
+    clf = MLPClassifier(random_state=0, max_iter=300).fit(vectors_train, twitter_cat[train_index])
     accuracies.append(metrics.accuracy_score(twitter_cat[test_index], clf.predict(vectors_test)))
 
 print("The accuracy of tfidf vectorizer without any pre-processing is: " + str(numpy.mean(accuracies)))
 
 accuracies = []
-clf = LogisticRegression(C=40.0, max_iter=1000, random_state=0)
 kf = KFold(n_splits=5, shuffle=True)
 for train_index, test_index in kf.split(twitter_text):
     vectors_train, vectors_test = count_vec_with_sw(twitter_text[train_index], twitter_text[test_index])
-    clf.fit(vectors_train, twitter_cat[train_index])
+    clf = MLPClassifier(random_state=0, max_iter=300).fit(vectors_train, twitter_cat[train_index])
     accuracies.append(metrics.accuracy_score(twitter_cat[test_index], clf.predict(vectors_test)))
 
 print("The accuracy of applying stopwords filter in the vectorization process is: " + str(numpy.mean(accuracies)))
 
 accuracies = []
-clf = LogisticRegression(C=40.0, max_iter=1000, random_state=0)
 kf = KFold(n_splits=5, shuffle=True)
 for train_index, test_index in kf.split(twitter_text):
     vectors_train, vectors_test = count_vec_with_ngram(twitter_text[train_index], twitter_text[test_index])
-    clf.fit(vectors_train, twitter_cat[train_index])
+    clf = MLPClassifier(random_state=0, max_iter=300).fit(vectors_train, twitter_cat[train_index])
     accuracies.append(metrics.accuracy_score(twitter_cat[test_index], clf.predict(vectors_test)))
 
 print("The accuracy of using unigram and bigram to do the vectorization process is: " + str(numpy.mean(accuracies)))
 
 accuracies = []
-clf = LogisticRegression(C=40.0, max_iter=1000, random_state=0)
 kf = KFold(n_splits=5, shuffle=True)
 for train_index, test_index in kf.split(twitter_text):
     vectors_train, vectors_test = count_vec_stem(twitter_text[train_index], twitter_text[test_index])
-    clf.fit(vectors_train, twitter_cat[train_index])
+    clf = MLPClassifier(random_state=0, max_iter=300).fit(vectors_train, twitter_cat[train_index])
     accuracies.append(metrics.accuracy_score(twitter_cat[test_index], clf.predict(vectors_test)))
 
 print("The accuracy of applying stem in the vectorization process is: " + str(numpy.mean(accuracies)))
 
 accuracies = []
-clf = LogisticRegression(C=40.0, max_iter=1000, random_state=0)
 kf = KFold(n_splits=5, shuffle=True)
 for train_index, test_index in kf.split(twitter_text):
     vectors_train, vectors_test = count_vec_lemma(twitter_text[train_index], twitter_text[test_index])
-    clf.fit(vectors_train, twitter_cat[train_index])
+    clf = MLPClassifier(random_state=0, max_iter=300).fit(vectors_train, twitter_cat[train_index])
     accuracies.append(metrics.accuracy_score(twitter_cat[test_index], clf.predict(vectors_test)))
 
 print("The accuracy of applying lemma in the vectorization process is: " + str(numpy.mean(accuracies)))
--- twitter_test_scrape.py
+++ twitter_test_scrape.py
@@ -26,22 +26,22 @@ tweets = tweepy.Cursor(api.search_tweets,
 # corona keywords in the positive set
 corona_keywords = np.loadtxt('./keywords.tsv', dtype=str, delimiter='\n')
 
-file = open('./dataset/neg/neg_tweets.csv', 'w', encoding='utf-8', newline='')
-file_info = open('./dataset/neg/info/neg_tweets_info.csv', 'w', encoding='utf-8', newline='')
-file_all = open('./dataset/neg/info/all_tweets.csv', 'w', encoding='utf-8', newline='')
+file = open('./dataset/test/testset_tweets.csv', 'w', encoding='utf-8', newline='')
+file_info = open('./dataset/test/info/testset_tweets_info.csv', 'w', encoding='utf-8', newline='')
 
 writer = csv.writer(file)
 writer_info = csv.writer(file_info)
-writer_all = csv.writer(file_all)
 
 for index, tweet in enumerate(tweets):
     # replace newline characters with space
     tweet_text = tweet.text.replace('\n', ' ')
-    if not any(keyword in tweet_text.lower() for keyword in corona_keywords):
-        writer.writerow([tweet_text])
-        writer_info.writerow([tweet_text, tweet.id, tweet.created_at, tweet.geo])
+    if any(keyword in tweet_text.lower() for keyword in corona_keywords):
+        writer.writerow([tweet_text, 1])
+        writer_info.writerow([tweet_text, 1, tweet.id, tweet.created_at, tweet.geo])
 
-    writer_all.writerow([tweet_text, tweet.id, tweet.created_at, tweet.geo])
+    else:
+        writer.writerow([tweet_text, 0])
+        writer_info.writerow([tweet_text, 0, tweet.id, tweet.created_at, tweet.geo])
+
 file.close()
 file_info.close()
-file_all.close()
